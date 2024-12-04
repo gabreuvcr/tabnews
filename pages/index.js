5821e464c4fd5163c61701bda2b847b5ba7e0919
Gabriel Rocha <gabreuvcr@gmail.com>
2024-12-04 00:20:28 +0000

    Modify phrase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Amanda, eu amo você. Se você me ama, da uma risadinha!</h1>
+    return <h1>Amanda, eu amo você. Se você me ama, da uma risadinha! 👨👩🐈🐈</h1>
 }
 
 export default Home;
