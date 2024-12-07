467088b7969db9675b2ff1c7475c1e76388060aa
Gabriel Rocha <gabreuvcr@gmail.com>
2024-12-07 03:31:22 +0000

    Add `lint:check` and `lint:fix` scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Amanda, eu amo você. Se você me ama, da uma risadinha! 👨👩🐈🐈</h1>
+  return (
+    <h1>Amanda, eu amo você. Se você me ama, da uma risadinha! 👨👩🐈🐈</h1>
+  );
 }
 
 export default Home;
